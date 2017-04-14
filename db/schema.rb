1d4b5cc86250b142359282bf35293d161b59d3a4
Abhishek Chandrasekhar <abhishek@glassbreakers.co>
2017-04-14 10:52:33 -0700

    Remove users table

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,4 @@ ActiveRecord::Schema.define(version: 20170101000000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "email", null: false
-    t.string "password"
-    t.string "password_salt"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
 end
