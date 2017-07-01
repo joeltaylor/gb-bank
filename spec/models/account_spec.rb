6477b0a2131a19af43400744861068dbd5a9e149
Joel Taylor <Joel@codeprivy.com>
2017-07-01 16:03:28 -0700

    Add Transaction association to Account

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   subject(:account) { Account.new }
 
+  it "has a default balance of 0.0" do
+    expect(account.balance).to eq(0.0)
+  end
+
   it "belongs to a member" do
     expect(account).to respond_to(:member)
   end
 
-  it "has a default balance of 0.0" do
-    expect(account.balance).to eq(0.0)
+  it "has many transactions" do
+    expect(account).to respond_to(:transactions)
   end
 end
