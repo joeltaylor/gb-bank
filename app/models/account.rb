65fc01ec6a66f18ecfcb81826596a3534cc662b6
Joel Taylor <Joel@codeprivy.com>
2017-07-02 19:03:00 -0700

    Add numeric validations to Account balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,8 @@
 class Account < ApplicationRecord
+  validates :balance, numericality: { greater_than_or_equal_to: -999_999.99,
+                                      less_than_or_equal_to: 999_999.99 }
+
   belongs_to :member
   has_many :transactions
+
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,8 +3,20 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   subject(:account) { Account.new }
 
-  it "has a default balance of 0.0" do
-    expect(account.balance).to eq(0.0)
+  it "has a default balance of 0.00" do
+    expect(account.balance).to eq(0.00)
+  end
+
+  it "is invalid with an amount greater than 999_999.99" do
+    account.balance = 1_000_000.00
+    expect(account).to_not be_valid
+    expect { account.save(validate: false) }.to raise_error(ActiveRecord::RangeError)
+  end
+
+  it "is invalid with an balance less than -999_999.99" do
+    account.balance = -1_000_000.00
+    expect(account).to_not be_valid
+    expect { account.save(validate: false) }.to raise_error(ActiveRecord::RangeError)
   end
 
   it "belongs to a member" do
