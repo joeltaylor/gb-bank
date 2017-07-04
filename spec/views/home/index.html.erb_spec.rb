3775f7fc7e82cde1fd3e7aa484d18a3d729e8353
Joel Taylor <Joel@codeprivy.com>
2017-07-04 13:53:28 -0700

    Add link to members page

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "home/index.html.erb", type: :view do
     render
     expect(page.find("h2")).to have_content(t("home.index.heading"))
   end
+
+  it "displays a link to view members" do
+    render
+    expect(page.find("a")).to have_content(t("home.index.members"))
+  end
 end
